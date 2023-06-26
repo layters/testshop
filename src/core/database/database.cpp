f9d25d166874272c9335ebf7b69e61033d330320
larteyoh <larteyoh@protonmail.com>
2023-06-26 07:03:25 -0400

    change data.sqlite3 location

--- src/core/database/database.cpp
+++ src/core/database/database.cpp
@@ -3,6 +3,6 @@
 neroshop::db::Sqlite3 * neroshop::get_database() {
     std::string database_path = NEROSHOP_DEFAULT_DATABASE_PATH;
     std::string database_file = "data.sqlite3";
-    static neroshop::db::Sqlite3 database_obj { database_file };////static neroshop::db::Sqlite3 database_obj { database_path + "/" + database_file };
+    static neroshop::db::Sqlite3 database_obj { database_path + "/" + database_file };
     return &database_obj;
 }
