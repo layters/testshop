254059de343c9bddb4256e8f6100c31d98d8018a
larteyoh <larteyoh@protonmail.com>
2024-07-01 02:37:27 +0000

    remove deprecated sha3 function

--- src/core/crypto/sha3.hpp
+++ src/core/crypto/sha3.hpp
@@ -9,7 +9,6 @@ namespace neroshop {
 
 namespace crypto {
     std::string sha3_256(const std::string& plain_text);
-    std::string sha3_256(const std::vector<uint8_t>& data);
     std::string sha3_512(const std::string& plain_text);
 }
 
