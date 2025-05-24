690907d379ccdca024169439f1cf5ad6df262ba1
layters <larteyoh@protonmail.com>
2025-05-24 03:54:07 +0000

    refresh buckets per hour

--- src/neroshop_config.hpp
+++ src/neroshop_config.hpp
@@ -38,7 +38,7 @@
 #define NEROSHOP_DHT_MAX_ROUTING_TABLE_NODES NEROSHOP_DHT_ROUTING_TABLE_BUCKETS * NEROSHOP_DHT_NODES_PER_BUCKET // = 5120
 #define NEROSHOP_DHT_MAX_HEALTH_CHECKS          3    // Maximum number of consecutive failed checks before marking the node as dead
 #define NEROSHOP_DHT_NODE_HEARTBEAT_INTERVAL    300  // Number of seconds between each node health check
-#define NEROSHOP_DHT_BUCKET_REFRESH_INTERVAL    900  // Number of seconds between each find_node query to neighboring nodes
+#define NEROSHOP_DHT_BUCKET_REFRESH_INTERVAL    3600 // Number of seconds between each find_node query to neighboring nodes
 #define NEROSHOP_DHT_DATA_REPUBLISH_INTERVAL    3600 // Number of seconds between each republishing of in-memory hash table data
 #define NEROSHOP_DHT_DATA_REMOVAL_INTERVAL      1800 // Number of seconds between each removal of all expired in-memory hash table data
 
